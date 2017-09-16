c3a0377cb55f691388a84811bef8f089ed648491
HertzDevil <nicetas.c@gmail.com>
2017-09-16 17:56:17 +0800

    groove.rb -> groove_real.rb, original script now produces dyadic grooves

--- groove_real.rb
+++ groove_real.rb
@@ -1,19 +1,21 @@
 #!/usr/bin/ruby
 
-DESC = 'BPM-to-groove converter for power-of-two sizes.
-Usage: ./groove.rb <bpm> [<option>...]
+DESC = 'High accuracy BPM-to-groove converter.
+Usage: ./groove_real.rb <bpm> [<option>...]
 Options:
 - `beat`: first highlight value (default 4)
 - `hz`: refresh rate (default 60)
+- `rows`: groove size, result groove must fit evenly
 - `max`: maximum groove size (default 16)
 - `ntsc`: equivalent to `60.0988 hz`
 - `pal`: equivalent to `50.007 hz`
 - `usec`: refresh interval in microseconds
 Examples:
-- `./groove.rb 120` => `8 7`
-- `./groove.rb 120 8 beat` => `4 4 4 3`
-- `./groove.rb 150 45 hz` => `5 4`
-- `./groove.rb 170 8 max` => `6 5 5 5`'
+- `./groove_real.rb 120` => `8 7`
+- `./groove_real.rb 120 8 beat` => `4 4 4 3`
+- `./groove_real.rb 150 45 hz` => `5 4`
+- `./groove_real.rb 165 4 rows` => `6 5`
+- `./groove_real.rb 170 8 max` => `6 5 5 6 5 5 5`'
 
 def get_args(t)
   args = {beat: 4, bpm: t.shift.to_f, hz: 60, max: 16}
@@ -40,7 +42,7 @@ def get_args(t)
       when 'usec'
         args[:hz] = 1000000.0 / last
         need_num = true
-      when 'bpm', 'beat', 'hz', 'max'
+      when 'bpm', 'beat', 'hz', 'rows', 'max'
         args[x.to_sym] = last
         need_num = true
       else
@@ -54,25 +56,26 @@ def get_args(t)
   args[:bpm] = last if !need_num
 
   args if args[:bpm] && args[:beat] % 1 == 0 &&
+    (!args[:rows] || args[:rows] % 1 == 0) &&
     args.all? {|k, v| v.is_a?(Numeric) && v > 0}
 end
 
-def make_groove(ticks, gsize)
-  return [ticks] if gsize == 1
-  make_groove(ticks - ticks / 2, gsize / 2) + make_groove(ticks / 2, gsize / 2)
-end
-
 def get_groove(params)
-  gsize = 2 ** Math.log2(params[:max]).to_i
-  total = (60 * params[:hz] / (params[:bpm] * params[:beat]) * gsize).round
-  while total.even? && gsize.even?
-    total, gsize = total / 2, gsize / 2
+  ticks = Rational(60 * params[:hz], params[:bpm] * params[:beat])
+  return [1] if ticks < 1
+  return [255] if ticks > 255
+
+  best = (1..params[:max]).map do |k|
+    next if params[:rows] && params[:rows] % k != 0
+    cycle = (ticks * k).round
+    [k, cycle, cycle / k.to_f]
+  end.compact.min do |a, b|
+    [(ticks - a[2]).abs, a[0]] <=> [(ticks - b[2]).abs, b[0]]
   end
 
-  g = make_groove(total, gsize)
-  return [1] if g.any? {|x| x < 1}
-  return [255] if g.any? {|x| x > 255}
-  g
+  (0...best[0]).map do |t|
+    (best[1] * (t + 1) - 1) / best[0] - (best[1] * t - 1) / best[0]
+  end
 end
 
 if ARGV.empty?
